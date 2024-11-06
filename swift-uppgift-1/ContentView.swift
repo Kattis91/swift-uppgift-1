13ef5cf6b4b5fd6c5809c1e47bb651d1dbbee342
Kattis91 <katya.svedmark@gmail.com>
2024-11-06 22:11:20 +0100

    Layout

--- swift-uppgift-1/ContentView.swift
+++ swift-uppgift-1/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+           
+            HStack (spacing: 0.00) {
+                
+                VStack {
+                    // RED
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150.0)
+                .background(Color.red)
+                
+                VStack {
+                    // GREEN
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150.0)
+                .background(Color.green)
+                
+                VStack {
+                    // YELLOW
+                }
+                .frame(maxWidth: .infinity, maxHeight: 150.0)
+                .background(Color.yellow)
+            }
+            
+            VStack {
+                // GRAY
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100.0)
+            .background(Color.gray)
+            
+            VStack {
+                // WHITE
+            }
+            .frame(width: 100.0, height: 100.0)
+            .background(Color.white)
+            
+            Spacer()
+            
+            
+            HStack {
+                // RED
+                
+                Spacer()
+                
+                VStack{
+                    // BLACK
+                }
+                .frame(width: 70.0, height: 70.0)
+                .background(Color.black)
+                .padding(35.00)
+            }
+            .frame(maxWidth: .infinity, maxHeight: 100.0)
+            .background(Color.red)
         }
-        .padding()
+        .frame(maxHeight: .infinity)
+        .background(Color.blue)
     }
 }
 
